9e3d152c34446657bbbf43212c184f559c658f4b
devadikhagesh <70067680+devadikhagesh@users.noreply.github.com>
2021-07-14 18:42:14 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -2,5 +2,5 @@
 sudo docker stop sample-app
 sudo docker rm sample-app
 sudo docker rmi sample-image
-sudo docker-compose -f docker-compose.yml up
+sudo docker-compose -f docker-compose.yml up -d
  
